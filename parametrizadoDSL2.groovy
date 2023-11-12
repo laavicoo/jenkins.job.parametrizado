875299b422d112146013c30a7d6a2f09a88de535
laavicoo <147121209+laavicoo@users.noreply.github.com>
2023-11-12 17:47:24 -0300

    Update parametrizadoDSL2.groovy

--- parametrizadoDSL2.groovy
+++ parametrizadoDSL2.groovy
@@ -20,22 +20,5 @@ job('ejemplo3-job-DSL') {
     	}
   	publishers {
       		mailer('macloujulian@gmail.com', true, true)
-      		slackNotifier {
-		  notifyAborted(true)
-		  notifyEveryFailure(true)
-		  notifyNotBuilt(false)
-		  notifyUnstable(false)
-		  notifyBackToNormal(true)
-		  notifySuccess(false)
-		  notifyRepeatedFailure(false)
-		  startNotification(false)
-		  includeTestSummary(false)
-		  includeCustomMessage(false)
-		  customMessage(null)
-		  sendAs(null)
-		  commitInfoChoice('NONE')
-		  teamDomain(null)
-		  authToken(null)
-        	}
     	}
 }
